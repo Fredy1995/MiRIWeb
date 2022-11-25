a0fd9141deaadec776adf38998195cc6911b14a3
fredy1995 <fredo199575@gmail.com>
2022-11-25 14:37:50 -0600

    Versión principal para producción

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiriWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //if (Session["idUser"] != null)
+            //{
+                return View();
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Login");
+            //}
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+      
+    }
+}
