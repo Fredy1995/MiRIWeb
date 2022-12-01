39f99d245caa7a5b673f387f362dfc4e6c44de6c
fredy1995 <fredo199575@gmail.com>
2022-12-01 17:16:15 -0600

    Login ya funciona

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,24 +6,49 @@ using System.Web.Mvc;
 
 namespace MiriWeb.Controllers
 {
+   
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            //if (Session["idUser"] != null)
-            //{
+            if (Session["idUser"] != null)
+            {
                 return View();
-            //}
-            //else
-            //{
-            //    return RedirectToAction("Login");
-            //}
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
         }
 
         public ActionResult Login()
         {
             return View();
         }
-      
+        [HttpPost]
+
+        public ActionResult Login(string user, string pass)
+        {
+            if (user.Equals("admin") && pass.Equals("1234"))
+            {
+                //add session
+                //Session["FullName"] = data.FirstOrDefault().nombre + " " + data.FirstOrDefault().aPaterno;
+                //Session["Email"] = data.FirstOrDefault().correo;
+                Session["idUser"] = 453;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.error = "Login failed";
+                return RedirectToAction("Login");
+            }
+        }
+        //Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
+        }
+
     }
 }
