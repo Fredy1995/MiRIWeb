79d15241d99007addeb112e4511e250738d4b9fe
fredy1995 <fredo199575@gmail.com>
2023-01-04 13:00:33 -0600

    Menu contextual ya se visualiza al hacer clic derecho en los temas

---
+++ Scripts/clicderecho.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+
+    //Ocultamos el menú al cargar la página
+    $("#menu_derecho").hide();
+    document.oncontextmenu = function () { return false }
+    /* mostramos el menú si hacemos click derecho con el ratón */
+    var div = $("#cuerpo").first();
+    div.contextmenu(function (e) {
+        $("#menu_derecho").css({ 'display': 'block', 'left': e.pageX, 'top': e.pageY });
+       
+    });
+
+    //$(document).bind("contextmenu", function (e) {
+    //    $("#menu_derecho").css({ 'display': 'block', 'left': e.pageX, 'top': e.pageY });
+    //    return false;
+    //});
+
+    var idSelec;
+    $('.nom_fic').mousedown(function (event) {
+        switch (event.which) {
+            case 3:
+                idSelec = $(this).attr("id");
+                break;
+        }
+    });
+
+    $('.nom_menu').click(function () {
+        alert("Has seleccionado " + $(this).html() + " sobre el fichero con id " + idSelec);
+    })
+
+
+    //cuando hagamos click, el menú desaparecerá
+    $(document).click(function (e) {
+        if (e.button == 0) {
+            $("#menu_derecho").css("display", "none");
+        }
+    });
+
+    //si pulsamos escape, el menú desaparecerá
+    $(document).keydown(function (e) {
+        if (e.keyCode == 27) {
+            $("#menu_derecho").css("display", "none");
+        }
+    });
+});
+
